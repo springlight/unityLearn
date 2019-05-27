e1a7e9849778abac322aef43a5d267e95ea97f47
springlight <118406814@qq.com>
2019-05-27 17:39:48 +0800

    ttt

---
+++ JAKASCANER/Assets/NewBehaviourScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NewBehaviourScript : MonoBehaviour {
+    public Button btn;
+    private AndroidJavaObject jo;
+    // Use this for initialization
+    void Start()
+    {
+        //获取Android的Java接口  
+        AndroidJavaClass jc = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
+        jo = jc.GetStatic<AndroidJavaObject>("currentActivity");
+        btn.onClick.AddListener(OnClickBtn);
+    }
+
+    public void OnClickBtn()
+    {
+        jo.Call("StartCan");
+    }
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
