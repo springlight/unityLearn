8f600f146e96ab28e9a10b2eeead89e667adf388
springlight <1184068184@qq.com>
2019-06-07 22:29:48 +0800

    configure spawn

---
+++ ObjectManager/Assets/Scripts/Editor/FloatRangeDrawer.cs
@@ -0,0 +1,15 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+public class FloatRangeDrawer : PropertyDrawer {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
