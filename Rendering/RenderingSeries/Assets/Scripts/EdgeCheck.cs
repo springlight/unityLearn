a62ac9f02663fab5bfac7a256c3c9f3b51dae920
springlight <118406814@qq.com>
2019-08-22 17:09:12 +0800

    edgecheck

---
+++ Rendering/RenderingSeries/Assets/Scripts/EdgeCheck.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EdgeCheck : PostEffectBase
+{
+    public Shader edgeCheckShader;
+    private Material edgeCheckMaterial;
+
+    [Range(0.0f, 1.0f)]
+    public float edgesOnly = 0.0f;
+    public Color edgeColor = Color.black;
+    public Color backgroundColor = Color.white;
+    public Material material
+    {
+        get
+        {
+            edgeCheckMaterial = CheckShaderAndCreateMaterial(edgeCheckShader, edgeCheckMaterial);
+            return edgeCheckMaterial;
+        }
+    }
+
+    private void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        if(material != null)
+        {
+            material.SetFloat("_EdgeOnly", edgesOnly);
+            material.SetColor("_EdgeColor", edgeColor);
+            material.SetColor("_BackgroundColor", backgroundColor);
+            Graphics.Blit(source, destination, material);
+        }
+        else
+        {
+            Graphics.Blit(source, destination);
+        }
+    }
+}
