48fc50fc15c057d952f57f411e1665b487ba2b10
springlight <118406814@qq.com>
2019-10-10 17:48:04 +0800

    move cureve

---
+++ MovingCurve/MovingCurve/Assets/LineMark.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LineMark : MonoBehaviour
+{
+
+
+    private int i;
+    public LineRenderer obs;
+    public GameObject run;
+    Vector3 RunStart;
+    Vector3 RunNext;
+
+    // Use this for initialization
+    void Start()
+    {
+        RunStart = run.transform.position;
+
+        i = 1;
+    }
+
+    // Update is called once per frame  
+    private float time = 0;
+    void Update()
+    {
+
+        RunNext = run.transform.position;
+       
+        if (RunStart != RunNext)
+        {
+           
+           
+            i++;
+            obs.SetVertexCount(i);//设置顶点数 
+            obs.SetPosition(i - 1, run.transform.position);
+
+        }
+
+        RunStart = RunNext;
+    }
+}
